c6f3a8ef099df7bc929c58dd8555f9bc7e1b494e
penroy99 <roy2.zichroni@gmail.com>
2023-04-30 13:24:32 +0300

    unsatisfied

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+function isPalindrome(a, b) {
+    if (a === b) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
-};
+ const palindromes = function (str) {
+    let newString = (str.toLowerCase()).replaceAll('.', ' ');
+    newString = newString.replaceAll(', ', ' ');
+    newString = newString.replaceAll(' ', '');
+    newString = newString.replaceAll('!', '');
+    let reversed = newString.split("").reverse().join("");
+    return isPalindrome(newString, reversed);
+}
 
 // Do not edit below this line
 module.exports = palindromes;
