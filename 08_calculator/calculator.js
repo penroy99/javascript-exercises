1fa6019b0d4c0d168e5ec7ed7a9afdf929fa39c7
penroy99 <roy2.zichroni@gmail.com>
2023-04-30 12:23:41 +0300

    calculator done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a ,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let value = 0;
+	for (let i = 0; i < array.length; i ++) {
+    value += array[i];
+  }
+  return value;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let value = 1;
+  for (let i = 0; i < array.length; i ++) {
+    value *= array[i];
+  }
+  return value;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) {
+    return 1;
+  } 
+  else {
+      let factArray = [];
+    let value = 1;
+    do {
+      factArray.unshift(num);
+      num -= 1;
+    }
+    while (num > 0);
+    for (let i = 0; i < factArray.length; i ++) {
+      value *= factArray[i];
+    }
+    return value;
+  }
 };
 
 // Do not edit below this line
