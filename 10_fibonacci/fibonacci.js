d08b180b2abf792ac273aee5f935d0b5ef4fc340
penroy99 <roy2.zichroni@gmail.com>
2023-05-01 10:23:51 +0300

    bummer

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    const n = parseInt(input);
+    let array = [0, 1];
+    if (n < 1) {
+        return "OOPS";
+    } else if (n === 1) {
+        return 1;
+    } else if (n === 2) {
+        return 1;
+    } else {
+        let i = 2;
+        while (n + 1 > i) {
+            array[i] = array[i-1] + array[i-2];
+            i ++;
+        }
+    }
+    return array[array.length - 1];
 };
 
 // Do not edit below this line
