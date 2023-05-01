ae814989c98e134e12c7f5288732ff4d2c27d2d4
penroy99 <roy2.zichroni@gmail.com>
2023-05-01 10:28:51 +0300

    by solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,21 +1,18 @@
-const fibonacci = function(input) {
-    const n = parseInt(input);
-    let array = [0, 1];
-    if (n < 1) {
-        return "OOPS";
-    } else if (n === 1) {
-        return 1;
-    } else if (n === 2) {
-        return 1;
-    } else {
-        let i = 2;
-        while (n + 1 > i) {
-            array[i] = array[i-1] + array[i-2];
-            i ++;
-        }
+const fibonacci = function(n) {
+    if (n < 0) {return "OOPS";};
+    if (n === 0) {return 0;};
+    let a = 0;
+    let b = 1;
+    for (let i = 1; i < n; i++) {
+        const temp = b;
+        b = a + b;
+        a = temp;
     }
-    return array[array.length - 1];
-};
+    return b;
+
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+  
